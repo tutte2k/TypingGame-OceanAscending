ded526014e5a1583fa762e249c877d8b969b8f6e
tutte2k <45877330+tutte2k@users.noreply.github.com>
2022-11-21 07:50:48 +0100

    init

---
+++ actors/Ghosty.js
@@ -0,0 +1,35 @@
+function Ghosty(text) {
+  this.position = createVector(random(100, width - 100), height + 100);
+  this.text = text;
+  this.completedText = "";
+  this.focused = false;
+  this.intact = true;
+  this.animation = GhostyAnimation;
+}
+
+Ghosty.prototype.draw = function () {
+  animation(this.animation, this.position.x, this.position.y);
+  noStroke();
+  let displayText = this.text;
+  displayText = this.text.replace(this.completedText, "");
+  textAlign(CENTER);
+  textSize(25);
+  text(displayText.toUpperCase(), this.position.x, this.position.y - 85);
+};
+Ghosty.prototype.update = function () {
+  this.position.y -= map(score, 0, 1000, 1, 15);
+  if (this.focused) {
+    this.position.x++;
+  }
+  if (this.position.y < 0) {
+    field.splice(field.indexOf(this), 1);
+    focus = null;
+  }
+};
+Ghosty.prototype.erode = function (keyCode) {
+  var inputChar = String.fromCharCode(keyCode).toLowerCase(); // keyCode to char
+  var length = this.completedText.length + 1;
+  if (this.text.substring(0, length) === this.completedText + inputChar)
+    this.completedText += inputChar;
+  this.intact = this.completedText !== this.text; // update intact
+};

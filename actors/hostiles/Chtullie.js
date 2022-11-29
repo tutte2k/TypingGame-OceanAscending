c93c7f4d207f56a9f13187fbc5790728073dd2e9
tutte2k <45877330+tutte2k@users.noreply.github.com>
2022-11-29 19:38:10 +0100

    sprite sheet fix

--- actors/hostiles/Chtullie.js
+++ actors/hostiles/Chtullie.js
@@ -10,48 +10,9 @@ class Chtullie extends Hostile {
     );
   }
   static loadAnimationFiles() {
-    Chtullie.Animation = loadAnimation(...Chtullie.Sprites);
+    Chtullie.Animation = loadAnimation(
+      "../../assets/sprites/chtullie/sheet_cthullie.png",
+      { size: [500, 500], frames: 48 }
+    );
   }
-  static Sprites = [
-    "./assets/sprites/chtullie/chtullie (1).png",
-    "./assets/sprites/chtullie/chtullie (2).png",
-    "./assets/sprites/chtullie/chtullie (3).png",
-    "./assets/sprites/chtullie/chtullie (4).png",
-    "./assets/sprites/chtullie/chtullie (5).png",
-    "./assets/sprites/chtullie/chtullie (6).png",
-    "./assets/sprites/chtullie/chtullie (7).png",
-    "./assets/sprites/chtullie/chtullie (8).png",
-    "./assets/sprites/chtullie/chtullie (9).png",
-    "./assets/sprites/chtullie/chtullie (10).png",
-    "./assets/sprites/chtullie/chtullie (11).png",
-    "./assets/sprites/chtullie/chtullie (12).png",
-    "./assets/sprites/chtullie/chtullie (13).png",
-    "./assets/sprites/chtullie/chtullie (14).png",
-    "./assets/sprites/chtullie/chtullie (15).png",
-    "./assets/sprites/chtullie/chtullie (16).png",
-    "./assets/sprites/chtullie/chtullie (17).png",
-    "./assets/sprites/chtullie/chtullie (18).png",
-    "./assets/sprites/chtullie/chtullie (19).png",
-    "./assets/sprites/chtullie/chtullie (20).png",
-    "./assets/sprites/chtullie/chtullie (21).png",
-    "./assets/sprites/chtullie/chtullie (22).png",
-    "./assets/sprites/chtullie/chtullie (23).png",
-    "./assets/sprites/chtullie/chtullie (24).png",
-    "./assets/sprites/chtullie/chtullie (25).png",
-    "./assets/sprites/chtullie/chtullie (26).png",
-    "./assets/sprites/chtullie/chtullie (27).png",
-    "./assets/sprites/chtullie/chtullie (28).png",
-    "./assets/sprites/chtullie/chtullie (29).png",
-    "./assets/sprites/chtullie/chtullie (30).png",
-    "./assets/sprites/chtullie/chtullie (31).png",
-    "./assets/sprites/chtullie/chtullie (32).png",
-    "./assets/sprites/chtullie/chtullie (33).png",
-    "./assets/sprites/chtullie/chtullie (34).png",
-    "./assets/sprites/chtullie/chtullie (35).png",
-    "./assets/sprites/chtullie/chtullie (36).png",
-    "./assets/sprites/chtullie/chtullie (37).png",
-    "./assets/sprites/chtullie/chtullie (38).png",
-    "./assets/sprites/chtullie/chtullie (39).png",
-    "./assets/sprites/chtullie/chtullie (40).png",
-  ];
 }
--- actors/hostiles/Shotty.js
+++ actors/hostiles/Shotty.js
@@ -5,31 +5,35 @@ class Shotty extends Hostile {
     super(text, Shotty.Animation, createVector(-30, 0));
   }
   static loadAnimationFiles() {
-    Shotty.Animation = loadAnimation(...Shotty.Sprites);
+    Shotty.Animation = loadAnimation(
+      "../../assets/sprites/bulletsquid/sheet_shotty.png",
+      { size: [100, 100], frames: 22 }
+    );
+    // Shotty.Animation = loadAnimation(...Shotty.Sprites);
   }
 
-  static Sprites = [
-    "./assets/sprites/bulletsquid/bulletsquid (1).png",
-    "./assets/sprites/bulletsquid/bulletsquid (2).png",
-    "./assets/sprites/bulletsquid/bulletsquid (3).png",
-    "./assets/sprites/bulletsquid/bulletsquid (4).png",
-    "./assets/sprites/bulletsquid/bulletsquid (5).png",
-    "./assets/sprites/bulletsquid/bulletsquid (6).png",
-    "./assets/sprites/bulletsquid/bulletsquid (7).png",
-    "./assets/sprites/bulletsquid/bulletsquid (8).png",
-    "./assets/sprites/bulletsquid/bulletsquid (9).png",
-    "./assets/sprites/bulletsquid/bulletsquid (10).png",
-    "./assets/sprites/bulletsquid/bulletsquid (11).png",
-    "./assets/sprites/bulletsquid/bulletsquid (12).png",
-    "./assets/sprites/bulletsquid/bulletsquid (13).png",
-    "./assets/sprites/bulletsquid/bulletsquid (14).png",
-    "./assets/sprites/bulletsquid/bulletsquid (15).png",
-    "./assets/sprites/bulletsquid/bulletsquid (16).png",
-    "./assets/sprites/bulletsquid/bulletsquid (17).png",
-    "./assets/sprites/bulletsquid/bulletsquid (18).png",
-    "./assets/sprites/bulletsquid/bulletsquid (19).png",
-    "./assets/sprites/bulletsquid/bulletsquid (20).png",
-    "./assets/sprites/bulletsquid/bulletsquid (21).png",
-    "./assets/sprites/bulletsquid/bulletsquid (22).png",
-  ];
+  // static Sprites = [
+  //   "./assets/sprites/bulletsquid/bulletsquid (1).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (2).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (3).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (4).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (5).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (6).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (7).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (8).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (9).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (10).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (11).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (12).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (13).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (14).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (15).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (16).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (17).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (18).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (19).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (20).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (21).png",
+  //   "./assets/sprites/bulletsquid/bulletsquid (22).png",
+  // ];
 }
